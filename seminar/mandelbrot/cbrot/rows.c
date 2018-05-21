4757a685c02ec2c833cee40837bd6162aab83b01
ludvigch <ludvigch@users.noreply.github.com>
2018-05-21 14:42:52 +0200

    removed unnecessary comment

--- seminar/mandelbrot/cbrot/rows.c
+++ seminar/mandelbrot/cbrot/rows.c
@@ -45,40 +45,6 @@ mandelbrot(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
     return enif_make_int(env, res);
 }
 
-/*
-static ERL_NIF_TERM
-mandelbrot(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    int width, height,  depth;
-    double k, x, y;
-    if(!enif_get_int(env, argv[0], &width)  ||
-       !enif_get_int(env, argv[1], &height) ||
-       !enif_get_double(env, argv[2], &x)   ||
-       !enif_get_double(env, argv[3], &y)   ||
-       !enif_get_double(env, argv[4], &k)   ||
-       !enif_get_int(env, argv[5], &depth))
-        return enif_make_badarg(env);
-
-    double[] trans = {x,y,k};
-
-
-}
-
-ERL_NIF_TERM[] rows(int width, int height, double[] trans, int depth){
-    ERL_NIF_TERM[] rows;
-    int i;
-    for(i = height; i >= 0; i--){
-
-    }
-}
-
-ERL_NIF_TERM[] row(int width, int height, double[] trans, int depth){
-    ERL_NIF_TERM[] row;
-    int i;
-    for(i=width; i >= 0; i--){
-
-    }
-}*/
-
 static ErlNifFunc nif_funcs[] = {
     {"mandelbrot", 3, mandelbrot}
 };
